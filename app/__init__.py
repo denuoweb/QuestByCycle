60d584fd10cf8325c9523a09cf9e8948c866b5d2
denuoweb <jaron@rosenau.info>
2025-04-11 20:40:33 -0700

    add local_domain config

--- app/__init__.py
+++ app/__init__.py
@@ -104,6 +104,8 @@ def create_app():
     app.config['INSTAGRAM_USER_ID'] = app.config['social']['instagram_user_id']
     app.config['SOCKETIO_SERVER_URL'] = app.config['socketio']['SERVER_URL']
 
+    app.config['LOCAL_DOMAIN'] = app.config['main']['LOCAL_DOMAIN']
+
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1, x_port=1)
 
     # Initialize extensions
