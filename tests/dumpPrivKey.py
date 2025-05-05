4b91e0b81ca6f61b528d184b9152bf4d5ed3af22
denuoweb <jaron@rosenau.info>
2025-05-05 04:05:15 -0700

    dump actor private key for tests

---
+++ tests/dumpPrivKey.py
@@ -0,0 +1,7 @@
+from app import create_app, db
+from app.models import User
+app = create_app()
+with app.app_context():
+    user = User.query.filter_by(username='t12').first()
+    with open('actor_priv.pem','w') as f:
+        f.write(user.private_key)
