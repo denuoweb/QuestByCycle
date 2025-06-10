e0251b881f77a872fb483dc41141e51fb53e80b4
denuoweb <jaron@rosenau.info>
2025-06-10 06:37:29 -0700

    increment sw version

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,5 @@
 // The version of the cache
-const VERSION = "v6"; // Update this version number when changes are made
+const VERSION = "v7"; // Update this version number when changes are made
 const CACHE_NAME = `questbycycle-${VERSION}`;
 
 // List of static resources to cache
