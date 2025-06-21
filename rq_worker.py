0cdaf86a132cf65c221b0c615d4dd552f6737ef0
denuoweb <jaron@rosenau.info>
2025-06-21 07:24:53 -0700

    remove legacy connection

--- rq_worker.py
+++ rq_worker.py
@@ -1,4 +1,4 @@
-from rq import Worker, Connection
+from rq import Worker
 
 from app import create_app
 from app.tasks import init_queue
@@ -7,11 +7,12 @@ from app.tasks import init_queue
 def main() -> None:
     app = create_app()
     queue = init_queue(app)
+    # push the Flask context so tasks can use current_app, etc.
     app.app_context().push()
 
-    with Connection(queue.connection):
-        worker = Worker([queue])
-        worker.work()
+    # create a Worker bound to this queue and its connection
+    worker = Worker([queue], connection=queue.connection)
+    worker.work()
 
 
 if __name__ == "__main__":
