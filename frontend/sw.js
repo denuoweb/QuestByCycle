4dd7b60077e2dbda84585315f8118353e886145a
denuoweb <jaron@rosenau.info>
2025-08-09 14:26:44 -0700

    switch color of index button

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,5 @@
 // The version of the cache
-const VERSION = '1.1.0-f0673ef'; // Update this version number when changes are made
+const VERSION = '1.1.2-44743f0'; // Update this version number when changes are made
 const CACHE_NAME = `questbycycle-${VERSION}`;
 
 // List of static resources to cache
